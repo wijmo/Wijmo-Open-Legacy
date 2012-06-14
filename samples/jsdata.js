feff7028b615bf5bd8549e1d3f2cb8914267cbf5
jebro <jonrcoulter@gmail.com>
2012-06-14 15:24:43 -0400

    Folder Cleanup

--- samples/jsdata.js
+++
@@ -1,1 +0,0 @@
-[{ "label": "Tokyo", "legendEntry": true, "fitType": "spline", "data": { "x": ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"], "y": [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6] }, "style": { "stroke": "red" }, "markers": { "visible": true, "type": "circle"}}]
